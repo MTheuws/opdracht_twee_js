7e35ede658a387ed12e707fefd36c14a304faf94
MTheuws <89788123+MTheuws@users.noreply.github.com>
2021-11-08 12:38:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Hello Winc Academy students")
+//Dit is een grote som
+/*let multiplication = 1230941 * 1823794;
+console.log("multiplication:", multiplication);
+let division = 637263 / 54;
+console.log("division:", division);*/
